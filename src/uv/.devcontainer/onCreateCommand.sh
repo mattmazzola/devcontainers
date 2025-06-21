92ec94baebdf534f019d5c13b43747346b6a7d89
Matthew Mazzola <mattmazzola@live.com>
2025-06-21 23:48:49 +0000

    Add uv devcontainer template

---
+++ src/uv/.devcontainer/onCreateCommand.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+set -ex
+
+echo "CONFIGURING GIT"
+git config --global safe.directory '*'
+git config --global core.editor "code --wait"
+git config --global pager.branch false
+
+echo "INSTALLING PROJECT DEPENDENCIES"
+
+# Uncomment after you have setup your UV project
+# uv sync
+
+echo "postCreateCommand.sh finished!"
---
+++ src/uv/.devcontainer/postStartCommand.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# Fill with commands you want to run each time you start container (open in VS code)
+
+echo "postStartCommand.sh finished!"
